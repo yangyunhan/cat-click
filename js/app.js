9fb14c934568e9cbcd9c70e7814dbb96e51b025b
yangyunhan <1205385190@qq.com>
2017-12-31 20:47:11 +0800

    refresh

--- js/app.js
+++ js/app.js
@@ -5,12 +5,7 @@ var ViewModel = function () {
     this.grade = ko.observable('Newborn');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
-
-    this.people = ko.observableArray([
-        { firstName: 'Bert', lastName: 'Bertington' },
-        { firstName: 'Charles', lastName: 'Charlesforth' },
-        { firstName: 'Denise', lastName: 'Dentiste' }
-    ]);
+    this.nicknames = ko.observableArray(['Tabtab','T-Bone','Mr.T','Etc']);
 
     this.incrementCounter = function () {
         this.clickCount(this.clickCount() + 1);
@@ -28,34 +23,3 @@ var ViewModel = function () {
 };
 
 ko.applyBindings(new ViewModel());
-
-
-function AppViewModel() {
-    var self = this;
-
-    self.p = ko.observableArray([
-        { n: 'Bert' },
-        { n: 'Charles' },
-        { n: 'Denise' }
-    ]);
-
-    self.addPerson = function() {
-        self.p.push({ n: "New at " + new Date() });
-    };
-
-    self.removePerson = function() {
-        self.p.remove(this);
-    }
-}
-
-ko.applyBindings(new AppViewModel());
-
-/*
-var viewModel = function () {
-    //this.months = ko.observableArray(['Jan','Feb','Mar']);
-    ko.applyBindings({
-        months: [ 'Jan', 'Feb', 'Mar', 'etc' ]
-    });
-};
-ko.applyBindings(new viewModel());
-*/
